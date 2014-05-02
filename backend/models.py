0d640873c3c8e25c5469c2590846574243af3d67
Tim Friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2014-05-02 13:39:02 +0200

    added initial models

--- backend/models.py
+++ backend/models.py
@@ -12,12 +12,12 @@ class Item(ndb.Model):
 
     def to_message(self):
         return ItemMessage(
-            title = self.title,
-            description = self.description,
-            expiration = self.expiration,
-            price = self.price,
-            owner = self.owner,
-            item_id = str(self.key.id()))
+            title=self.title,
+            description=self.description,
+            expiration=self.expiration,
+            price=self.price,
+            owner=self.owner,
+            item_id=str(self.key.id()))
 
     @classmethod
     def to_message_collection(Item, items):
@@ -26,6 +26,50 @@ class Item(ndb.Model):
             itemMessages.append(item.to_message())
 
         return ItemMessageCollection(
-            code = "OK",
-            message = "OK",
-            data = itemMessages)
+            code="OK",
+            message="OK",
+            data=itemMessages)
+
+
+class User(ndb.Model):
+    email = ndb.StringProperty(indexed=False)
+    name = ndb.StringProperty(indexed=False)
+    description = ndb.StringProperty(indexed=False)
+    image_url = ndb.StringProperty(indexed=False)
+    tag = ndb.StringProperty(indexed=False)
+    disabled = ndb.StringProperty(indexed=False)
+
+
+class Comm(ndb.Model):
+    subject = ndb.StringProperty(indexed=False)
+    sender = ndb.StringProperty(indexed=False)
+    receiver = ndb.StringProperty(indexed=False)
+    timestamp = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    item_id = ndb.StringProperty(indexed=False)
+    item_title = ndb.StringProperty(indexed=False)
+    price = ndb.StringProperty(indexed=False)
+
+
+class Transaction(ndb.Model):
+    closed = ndb.StringProperty(indexed=False)
+    buyer_confirmation = ndb.StringProperty(indexed=False)
+    seller_confirmation = ndb.StringProperty(indexed=False)
+    seller = ndb.StringProperty(indexed=False)
+    item_id = ndb.StringProperty(indexed=False)
+    price = ndb.StringProperty(indexed=False)
+    item_title = ndb.StringProperty(indexed=False)
+    transaction_id = ndb.StringProperty(indexed=False)
+    feedback_item = ndb.StringProperty(indexed=False)
+    feedback_seller = ndb.StringProperty(indexed=False)
+
+
+class Feedback(ndb.Model):
+    feedback_id = ndb.StringProperty(indexed=False)
+    author = ndb.StringProperty(indexed=False)
+    rating = ndb.StringProperty(indexed=False)
+    comment = ndb.StringProperty(indexed=False)
+    item_id = ndb.StringProperty(indexed=False)
+    seller_uid = ndb.StringProperty(indexed=False)
+    transaction_id = ndb.StringProperty(indexed=False)
+
